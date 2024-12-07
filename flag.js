fc301a36e5bbbe2b75c3dea2c3285e1ce95fe03b
erradical64 <asa@nbenson.com>
2024-12-06 17:47:44 -0700

    Added flag animations

--- flag.js
+++ flag.js
@@ -1,4 +1,34 @@
-import jsonData from 'countries.json' assert {type : 'json'};
+//import jsonData from 'countries.json' assert {type : 'json'};
 
-console.log(jsonData);
+//console.log(jsonData);
 //JSON.parse();
+
+async function getData() {
+  const url = "https://github.com/TheBenAtBrigham/BookingWebsite/blob/master/countries.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const myElement = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    myElement.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  myElement.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
